140ea4912412f31eb3415ecfd7b0b1fcd7663b9e
nera <grgicnera@gmail.com>
2026-01-26 08:01:55 +0100

    Initial commit

---
+++ src/main/java/com/retrosocial/app/RetroSocialApplication.java
@@ -0,0 +1,13 @@
+package com.retrosocial.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retrosocial/app/RetroSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retrosocial.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
